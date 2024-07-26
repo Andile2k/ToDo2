2d3cac84d3cc2d9dc26801da00edbc97cc9aeb23
Andile <andile.msomi64@gmail.com>
2024-07-26 08:49:45 +0200

    Updated

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { TodohomeComponent } from './todohome/todohome.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'home', component: TodohomeComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/todohome/todohome.component.ts
+++ src/app/todohome/todohome.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-todohome',
   templateUrl: './todohome.component.html',
   styleUrls: ['./todohome.component.scss']
 })
-export class TodohomeComponent {
+export class TodohomeComponent implements OnInit {
+
+  constructor() {}
+    ngOnInit(): void {
+    }
+    
+    onSubmit(form: NgForm) {
+      
+    }
+  
 
 }
