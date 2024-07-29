b84f14357cff16caab95fae826e2a5fa5d0d9e0a
Andile <andile.msomi64@gmail.com>
2024-07-29 15:45:38 +0200

    Creating click events for two buttons

--- src/app/todohome/todohome.component.ts
+++ src/app/todohome/todohome.component.ts
@@ -7,13 +7,26 @@ import { NgForm } from '@angular/forms';
   styleUrls: ['./todohome.component.scss']
 })
 export class TodohomeComponent implements OnInit {
+  taskArray = [{ taskName: 'Brush teeth', isCompleted: false}];
 
   constructor() {}
     ngOnInit(): void {
     }
     
     onSubmit(form: NgForm) {
-      
+      console.log(form)
+
+      this.taskArray.push({
+        taskName: form.controls['task'].value,
+        isCompleted: false
+      })
+    }
+
+    onDelete(index: number) {
+      console.log(index);
+
+      this.taskArray.splice(index, 1);
+
     }
   
 
